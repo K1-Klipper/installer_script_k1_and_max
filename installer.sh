81373ef2388e66c96e946d2d7d0f1e476d8f7cc7
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-12-28 10:33:07 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,11 +11,11 @@ case $choice in
 		git clone https://github.com/K1-Klipper/klipper.git /usr/data/klipper
 		mv /usr/share/klipper /usr/share/old.klipper
 		ln -s /usr/data/klipper /usr/share/klipper
-  		cp /usr/data/printer_data/configs/printer.cfg /usr/data/printer_data/configs/printer.bak
-		cp /usr/data/printer_data/configs/gcode_macro.cfg /usr/data/printer_data/config/gcode_macro.bak
-    		sed '/^[bl24c16f]$/,/^$/d' /usr/data/printer_data/configs/printer.cfg
-      		sed '/^square_corner_max_velocity: 200.0$/d' /usr/data/printer_data/configs/printer.cfg
-			sed '/\[include printer_params.cfg\]$/a\[include start_macro.cfg\]' /usr/data/printer_data/configs/printer.cfg
+  		cp /usr/data/printer_data/config/printer.cfg /usr/data/printer_data/configs/printer.bak
+		cp /usr/data/printer_data/config/gcode_macro.cfg /usr/data/printer_data/config/gcode_macro.bak
+    		sed '/^[bl24c16f]$/,/^$/d' /usr/data/printer_data/config/printer.cfg
+      		sed '/^square_corner_max_velocity: 200.0$/d' /usr/data/printer_data/config/printer.cfg
+			sed '/\[include printer_params.cfg\]$/a\[include start_macro.cfg\]' /usr/data/printer_data/config/printer.cfg
 			sed '/\[gcode_macro START_PRINT\]/,/CX_PRINT_DRAW_ONE_LINE/d' /usr/data/printer_data/config/gcode_macro.cfg
 			sed 's/CXSAVE_CONFIG/SAVE_CONFIG/g' /usr/data/printer_data/config/gcode_macro.cfg
 			file_to_check="/usr/data/printer_data/config/KAMP_settings.cfg"
@@ -43,8 +43,8 @@ case $choice in
                 echo "Uninstalling..."
                 rm /usr/share/klipper
 		mv /usr/share/old.klipper /usr/share/klipper
-  		mv /usr/data/printer_data/configs/printer.bak /usr/data/printer_data/configs/printer.cfg
-  		mv /usr/data/printer_data/configs/gcode_macro.bak /usr/data/printer_data/configs/gcode_macro.cfg
+  		mv /usr/data/printer_data/config/printer.bak /usr/data/printer_data/configs/printer.cfg
+  		mv /usr/data/printer_data/config/gcode_macro.bak /usr/data/printer_data/configs/gcode_macro.cfg
 		/etc/init.d/S55klipper_service restart
 		;;
             no|NO)
