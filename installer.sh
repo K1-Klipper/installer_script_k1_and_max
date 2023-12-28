ce68337e3bd14bfbf7ff5e8cef4d2a8fcdfb1f41
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-12-28 10:34:57 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -15,16 +15,17 @@ case $choice in
 		cp /usr/data/printer_data/config/gcode_macro.cfg /usr/data/printer_data/config/gcode_macro.bak
     		sed '/^[bl24c16f]$/,/^$/d' /usr/data/printer_data/config/printer.cfg
       		sed '/^square_corner_max_velocity: 200.0$/d' /usr/data/printer_data/config/printer.cfg
-			sed '/\[include printer_params.cfg\]$/a\[include start_macro.cfg\]' /usr/data/printer_data/config/printer.cfg
 			sed '/\[gcode_macro START_PRINT\]/,/CX_PRINT_DRAW_ONE_LINE/d' /usr/data/printer_data/config/gcode_macro.cfg
 			sed 's/CXSAVE_CONFIG/SAVE_CONFIG/g' /usr/data/printer_data/config/gcode_macro.cfg
 			file_to_check="/usr/data/printer_data/config/KAMP_settings.cfg"
                 if [ -f "$file_to_check" ]; then
                     echo "Found KAMP installing start macro for KAMP"
-                    wget -P /usr/data/printer_data/config/start_macro.cfg https://github.com/K1-Klipper/installer_script_k1_and_max/raw/main/start_macro_KAMP.cfg 
+		    sed '/\[include printer_params.cfg\]$/a\[include start_macro_KAMP.cfg\]' /usr/data/printer_data/config/printer.cfg
+                    wget -P /usr/data/printer_data/config/ https://github.com/K1-Klipper/installer_script_k1_and_max/raw/main/start_macro_KAMP.cfg 
                 else
                     echo "KAMP not found. Installing normal start macro"
-                    wget -P /usr/data/printer_data/config/start_macro.cfg https://github.com/K1-Klipper/installer_script_k1_and_max/raw/main/start_macro.cfg
+		    sed '/\[include printer_params.cfg\]$/a\[include start_macro.cfg\]' /usr/data/printer_data/config/printer.cfg
+                    wget -P /usr/data/printer_data/config/ https://github.com/K1-Klipper/installer_script_k1_and_max/raw/main/start_macro.cfg
                 fi
 		/etc/init.d/S55klipper_service restart
                 ;;
