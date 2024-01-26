b7be134b403463a333e56af4b450cbc9b55d189e
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2024-01-26 09:24:51 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -25,6 +25,7 @@ case $choice in
 		/usr/share/klippy-env/bin/python3 -m compileall /usr/data/klipper/klippy
   		mv /etc/init.d/S55klipper_service /usr/data/S55klipper_service.bak
     		wget --no-check-certificate -P /etc/init.d/ https://raw.githubusercontent.com/K1-Klipper/installer_script_k1_and_max/main/S55klipper_service
+		chmod +x /etc/init.d/S55klipper_service
 		/etc/init.d/S55klipper_service restart
                 ;;
             no|NO)
